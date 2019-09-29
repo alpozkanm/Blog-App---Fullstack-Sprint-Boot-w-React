b7a6b29f3b73ea532552ef0710878acd60f5d7ba
Alp Ozkan <alpozkanm@Alps-MacBook-Pro.local>
2019-09-29 23:24:28 +0300

    First commit to backend

---
+++ spring-boot-fullstack-crud-full-stack-with-maven/src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/course/CourseResource.java
@@ -0,0 +1,20 @@
+package com.in28minutes.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CourseResource {
+    @Autowired
+    private CoursesHardcodedService courseManagementService;
+
+    @GetMapping("/instructors/{username}/courses")
+    public List<Course> getAllCourses(@PathVariable String username) {
+        return courseManagementService.findAll();
+    }
+
+}
